4fe22140c864543727ed4a3fc0f38104d95bda06
nurtayzh <nurtay_zh@mail.ru>
2022-08-31 10:26:59 +0200

    revert rename project

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,10 +16,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'portfolio'`, () => {
+  it(`should have as title 'demo-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('portfolio');
+    expect(app.title).toEqual('demo-app');
   });
 
   it('should render title', () => {
@@ -27,7 +27,7 @@ describe('AppComponent', () => {
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('.content span')?.textContent).toContain(
-      'portfolio app is running!'
+      'demo-app app is running!'
     );
   });
 });
